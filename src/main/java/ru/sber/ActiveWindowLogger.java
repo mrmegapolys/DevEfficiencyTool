65b1545583853d8df5003f12bd05aeef2e507708
mrmegapolys <49758124+mrmegapolys@users.noreply.github.com>
2020-02-06 22:57:02 +0300

    ActiveWindowLogger mvp (#1)
    * added writing to file instead of console
    
    * added PID tracking

--- src/main/java/ru/sber/ActiveWindowLogger.java
+++ src/main/java/ru/sber/ActiveWindowLogger.java
@@ -2,12 +2,17 @@ package ru.sber;
 
 import com.sun.jna.Native;
 import com.sun.jna.platform.win32.WinDef.HWND;
+import com.sun.jna.ptr.IntByReference;
+
+import java.util.logging.Logger;
 
 import static com.sun.jna.platform.win32.User32.INSTANCE;
+import static java.util.logging.Logger.getLogger;
 
 public class ActiveWindowLogger {
     private final int MAX_TITLE_LENGTH = 1024;
     private final char[] buffer = new char[MAX_TITLE_LENGTH];
+    private final Logger log = getLogger(this.getClass().getName());
 
     public void start(long pollingDelay) {
         new Thread(() -> run(pollingDelay)).start();
@@ -15,15 +20,25 @@ public class ActiveWindowLogger {
 
     private void run(long pollingDelay) {
         while (true) {
-            write(getActiveWindowName());
+            log.info(getActiveWindowInfo());
             sleep(pollingDelay);
         }
     }
 
-    private String getActiveWindowName() {
+    private String getActiveWindowInfo() {
         HWND hwnd = INSTANCE.GetForegroundWindow();
+        return String.format("TITLE: %s; PID: %s", getActiveWindowTitle(hwnd), getActiveWindowPID(hwnd));
+    }
+
+    private String getActiveWindowTitle(HWND hwnd) {
         INSTANCE.GetWindowText(hwnd, buffer, MAX_TITLE_LENGTH);
         return Native.toString(buffer);
+    }
+
+    private Integer getActiveWindowPID(HWND hwnd) {
+        IntByReference pidRef = new IntByReference();
+        INSTANCE.GetWindowThreadProcessId(hwnd, pidRef);
+        return pidRef.getValue();
     }
 
     private void sleep(long millis) {
@@ -32,9 +47,5 @@ public class ActiveWindowLogger {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private void write(String str) {
-        System.out.println(str);
     }
 }
